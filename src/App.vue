9c531268cfab43b34c15646f14a0ea2ef40beadd
dangibson22 <dan.w.gibson.22@gmail.com>
2019-11-20 17:13:43 -0500

    Added basic Vue setup. Currently features one page that doesn't load properly run yarn install to get all packages updated

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-app>
+        <v-content>
+            <v-container> <router-view></router-view> </v-container>
+        </v-content>
+        <v-footer class="pa-3">
+            <v-layout class="font-weight-thin">that's really sad</v-layout>
+        </v-footer>
+    </v-app>
+</template>
+
+<script>
+    console.log("loaded");
+
+    export default {
+        name: "App",
+        components: {
+
+        }
+    };
+</script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-container>
+        <div>
+            <h4 class="display-1">Honestly, welcome to my website</h4>
+
+            <p class="body-1">something something award homepage</p>
+        </div>
+    </v-container>
+</template>
