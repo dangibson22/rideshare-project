d8b0a51fefff637829f47dbac6fcebddb074b6cc
dangibson22 <dan.w.gibson.22@gmail.com>
2019-11-22 09:48:49 -0500

    Updated package.json file that has all needed components to run the site server using yarn api-watch and yarn ui-watch

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import vuetify from "./plugins/vuetify";
+import vuetify from "./plugins/Vuetify.js";
 import router from "./router";
 import axios from "axios";
 import App from "./App.vue";
